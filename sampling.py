ff646bc42d86bfa2182712aae43427c4f8e8d434
santa-sukitoku <santanaruse@gmail.com>
2019-11-28 14:48:45 +0900

    pyaudioの基本情報を記述

--- sampling.py
+++ sampling.py
@@ -1,2 +1,17 @@
 import pyaudio
 import wave
+
+RECORD_SECONDS = 10
+WAVE_OUTPUT_FILENAME = 'sample.wav'
+iDeviceIndex = 0
+
+FORMAT = pyaudio.pyInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 2 ** 11
+audio = pyaudio.PyAudio()
+
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    input_device_index=iDeviceIndex,
+                    frames_per_buffer=CHUNK)
