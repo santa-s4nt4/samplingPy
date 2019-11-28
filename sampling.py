8add1196f9ab307bc4fcee914a5c55fc8c0b036d
santa-sukitoku <santanaruse@gmail.com>
2019-11-28 15:04:06 +0900

    録音機能の実装完了

--- sampling.py
+++ sampling.py
@@ -1,11 +1,11 @@
 import pyaudio
 import wave
 
-RECORD_SECONDS = 10
+RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = 'sample.wav'
 iDeviceIndex = 0
 
-FORMAT = pyaudio.pyInt16
+FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 CHUNK = 2 ** 11
@@ -15,3 +15,22 @@ stream = audio.open(format=FORMAT, channels=CHANNELS,
                     rate=RATE, input=True,
                     input_device_index=iDeviceIndex,
                     frames_per_buffer=CHUNK)
+
+print('recording...')
+frames = []
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print('finished recording!')
+
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
