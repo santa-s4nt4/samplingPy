003d7e5ca2dddcbc4903e575d54f7110464bc3df
santa-sukitoku <santanaruse@gmail.com>
2019-12-02 16:25:05 +0900

    python-oscの削除

--- sampling.py
+++ sampling.py
@@ -3,6 +3,7 @@ import wave
 from pythonosc import osc_server
 from pythonosc.dispatcher import Dispatcher
 
+
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = 'sample.wav'
 iDeviceIndex = 0
@@ -21,7 +22,7 @@ stream = audio.open(format=FORMAT, channels=CHANNELS,
 input('Press Any key to record for 5 seconds. Please tell me your name.')
 print('Recording...')
 frames = []
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+for i in range(1, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
     frames.append(data)
 
@@ -30,7 +31,6 @@ print('Finished recording!')
 stream.stop_stream()
 stream.close()
 audio.terminate()
-
 waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
 waveFile.setnchannels(CHANNELS)
 waveFile.setsampwidth(audio.get_sample_size(FORMAT))
